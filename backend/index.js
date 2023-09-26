f3b0d513d3b79009b0733864e90137ac5761ff2b
JadeRoss <banagjadeross@gmail.com>
2023-09-26 20:14:44 +0800

    all changes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,35 @@
 import express from "express";
 import { PORT,mongoDBURL } from './config.js';
 import mongoose from "mongoose";
+import cors from "cors"
+ 
+import booksRoute from "./routes/booksRoute.js"
 
 const app = express();
+// Middleware for parsing request body
+app.use(express.json())
+
+
+// Middleware for handling CORS Policy
+// Option 1: Allow all origin with default of cors
+app.use(cors())
+// Option 2: Allow Custom Origins
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000",
+//         methods: ['GET','POST','DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// )
+
 
 app.get('/',(req,res)=>{
     console.log(req);
     return res.status(234).send('Welcome to Book Store')
 });
+
+
+app.use('/books',booksRoute)
 
 
 
